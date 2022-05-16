e7581cfa6fb015e6dacbb524cc7a1636bf2ff8bc
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-05-16 15:36:16 +0530

    Add files via upload

--- tri.sh
+++ tri.sh
@@ -13,22 +13,23 @@ then
 else
     echo "not a valid traingle"
 fi
-#ksb@ksb-ThinkCentre-Edge72:~$ sh tri.sh
-#enter  side A
-#3
-#enter side B
-#0
-#enter side C
-#9
-#not a valid traingle
-#ksb@ksb-ThinkCentre-Edge72:~$ 3
-#3: command not found
-#ksb@ksb-ThinkCentre-Edge72:~$ sh tri.sh
-#enter  side A
-#3
-#enter side B
-#4
-#enter side C
-#5
-#valid traingle
 
+<<com
+ksb@ksb-ThinkCentre-Edge72:~$ sh tri.sh
+enter  side A
+3
+enter side B
+0
+enter side C
+9
+not a valid traingle
+
+$ sh tri.sh
+enter  side A
+3
+enter side B
+4
+enter side C
+5
+valid traingle
+com
