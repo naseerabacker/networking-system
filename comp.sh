5f2de74e6b846af2bf00c70589ec7f95e4452eab
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-05-25 11:42:29 +0530

    Add files via upload

---
+++ comp.sh
@@ -0,0 +1,26 @@
+#Shell script to compare two files. If the files are identical delete one
+
+echo "enter filename 1:"
+read f1
+echo "enter filename 2:"
+read f2
+cmp $f1 $f2
+if [ $? -eq 0 ]
+then
+	rm $f2
+	echo "second file removed"
+	exit
+fi
+echo "files not identical"
+
+<<com
+ksb@ksb-ThinkCentre-Edge72:~/Desktop/reer$ sh comp.sh
+enter filename 1:
+EXAM2
+enter filename 2:
+pp
+cmp: EXAM2: Is a directory
+files not identical
+ksb@ksb-ThinkCentre-Edge72:~/Desktop/reer$ 
+
+com
