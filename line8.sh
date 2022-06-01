bc506a08a22a7efed3de5f2fe6b3e94bcda68cf4
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-06-01 11:51:31 +0530

    Add files via upload

---
+++ line8.sh
@@ -0,0 +1,39 @@
+# Shell script to display a file from last line to first line
+
+if [ $# -ne 1 ]
+then
+	echo "syntax error"
+	echo " <$0> <filename>"
+	exit
+fi
+
+file=$1
+ter=`tty`
+exec < $file
+c=0
+while read line
+do
+c=`expr $c + 1`
+done
+exec < $ter
+
+while [ 0 -le $c ]
+do
+cat $file | head -$c | tail -n +$c
+c=`expr $c - 1`
+done 
+
+
+<<com
+ksb@ksb-ThinkCentre-Edge72:~/Desktop/net script$ sh line8.sh abnm
+animal
+umbrella
+sat
+cat
+ksb@ksb-ThinkCentre-Edge72:~/Desktop/net script$ sh line8.sh 
+syntax error
+ <line8.sh> <filename>
+com
+ksb@ksb-ThinkCentre-Edge72:~/Desktop/net script$ 
+
+com
