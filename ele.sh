688d94cecd3afbb4b5094cd5dbd5917191bcfb67
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-06-06 03:13:17 -0700

    Add files via upload

--- ele.sh
+++ ele.sh
@@ -1,4 +1,4 @@
-
+# Shell script to print specified range of lines from given files
 if [ $# -ne 3 ]
 then
     echo "syntax error"
@@ -9,7 +9,7 @@ fi
 file=$1
 l=$2
 u=$3
-echo "\n----------------------"
+echo "---------------------"
 echo "lines between $l - $u is printed below\n"
 
 cat $file | head -$u | tail -n +$l
